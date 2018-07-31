a456d36b6d25eb7c04a48b896229ad8b8ebc539f
Zafroyal <zafroyalptyltd@gmail.com>
2018-08-01 01:59:38 +0200

    'Basic cart concept without persistance included'

--- resources/assets/js/components/CartComponent.vue
+++ resources/assets/js/components/CartComponent.vue
@@ -1,56 +1,68 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card card-default">
-                    <div class="card-header">Cart</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card card-default">
+                <div class="card-header">Menu</div>
 
-                    <div class="card-body">
-                      <table class="table table-striped">
-                        <thead>
-                          <tr>
+                <div class="card-body">
+                <div class="card card-default" >
 
-                            <th scope="col">Item</th>
-                            <th scope="col">Price</th>
-                            <th scope="col">Quantity</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
+                  <table class="table table-bordered">
+                    <thead>
+                          <th scope="col">Product</th>
+                          <th scope="col">Price</th>
+                          <th scope="col">Quantity</th>
 
-                            <td>Cheese Burger</td>
-                            <td>R 28</td>
-                            <td>4</td>
-                          </tr>
-                          <tr>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr v-for="(product, index) in products">
 
-                            <td>Ham cheese and tomato</td>
-                            <td>R 20</td>
-                            <td>2</td>
-                          </tr>
-                          <tr>
-                            <td>Monster</td>
-                            <td>R 16</td>
-                            <td>3</td>
-                          </tr>
-                        </tbody>
-                      </table>
+                          <td>{{product.name}}</td>
+                          <td>{{product.price}}</td>
+                          <td>2</td>
+                          <button v-on:click="addProduct (index)"  class="btn btn-danger col-md-2">
+                              Add
+                          </button>
+                        </tr>
+                      </tbody>
+                    </table>
 
-                      <button style="position: relative; left:0%;"type="submit" class="btn btn-danger">
-                          Check Out
-                      </button>
-                    </div>
+
+                </div>
                 </div>
             </div>
         </div>
-
     </div>
+
+</div>
 </template>
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
+        data() {
+          return {
+
+              products: [
+                {
+                  name: 'Cheese',
+                  price: 20,
+                  quant: 5
+                },
+                {
+                  name: 'Ham',
+                  price: 28,
+                  quant: 3
+                }
+              ],
+              counter: 0
+
+          }
+        },
+        methods: {
+
         }
+
     }
 </script>
--- resources/assets/js/components/HomeComponent.vue
+++ resources/assets/js/components/HomeComponent.vue
@@ -4,16 +4,22 @@
             <div class="col-md-12">
                 <div class="card card-default">
                     <div class="card-header">Menu</div>
+                    Cart
+                    <div v-for="(product, index) in cartdata">
+                      {{product.name}}
+                    </div>
+                    <br/>
+                     R {{total}}
 
                     <div class="card-body" v-for="(products, index) in filteredProducts">
                     <div class="card card-default" >
 
 
                         <div class="card-body">
-                          <h2 style="padding:0px;">{{products.name}}   R{{products.price}} </h2>
+                          <h2 style="padding:0px;">{{products.name}}   R{{products.price}}  </h2>
 
                           <p>{{products.description}}</p>
-                          <button type="submit" class="btn btn-danger">
+                          <button type="submit" v-on:click="addProduct (products)" class="btn btn-danger">
                               Add to cart
                           </button>
                         </div>
@@ -32,18 +38,60 @@
 <script>
 export default {
       data:function() {
-          return {products: ''};
+          return {
+            products: [],
+            cartdata: [
+
+            ]
+          };
       },
+
       created: function(){
-        let uri = 'http://127.0.0.1:8000/proapi/';
+        let uri = 'http://localhost:8000/proapi/';
         Axios.get(uri).then((response) => {
           this.products = response.data;
         });
+
+
+
       },
       computed: {
         filteredProducts: function(){
             return this.products;
+        },
+
+        total: function(){
+
+          var tot = 0;
+
+
+          this.cartdata.forEach(product => tot += product.price)
+
+          return tot;
         }
+
+      },
+
+
+      methods: {
+
+        addProduct: function(product){
+
+
+              this.cartdata.push({
+                id: product.id,
+                name: product.name,
+                price: product.price,
+                description: product.description
+
+
+              })
+
+
+        }
+      },
+
+      mounted(){
 
       }
 
